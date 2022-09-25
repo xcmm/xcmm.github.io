58dfd305a9eaba8bcd747905d754c3ba43a8462a
xcmm <wx-up@qq.com>
2022-09-25 19:25:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# xcmm.github.io
---
+++ static/js/m8jsico.js
@@ -0,0 +1,20 @@
+document.writeln("<link href=\"/favicon.ico\" rel=\"icon\" type=\"image/x-icon\" />");
+document.writeln("<link href=\"/favicon.ico\" rel=\"shortcut icon\" type=\"image/x-icon\" />");
+document.writeln("<link href=\"/favicon.ico\" rel=\"bookmark\" type=\"image/x-icon\" />");
+
+document.writeln("<script>");
+document.writeln("var _hmt = _hmt || [];");
+document.writeln("(function() {");
+document.writeln("  var hm = document.createElement(\'script\');");
+document.writeln("  hm.src = \'https://hm.baidu.com/hm.js?d3512068a9151609a322d472367a5997\';");
+document.writeln("  var s = document.getElementsByTagName(\'script\')[0]; ");
+document.writeln("  s.parentNode.insertBefore(hm, s);");
+document.writeln("})();");
+document.writeln("</script>");
+document.writeln("");
+
+document.writeln("<script language=javascript>");
+document.writeln("  if(screen.width>=\'1081\')");
+document.writeln("  window.location = \'http://www.1213.me/i/\';");
+document.writeln("else{}");
+document.writeln("</script>");
